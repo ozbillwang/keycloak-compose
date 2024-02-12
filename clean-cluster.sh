6084f4a610b8fb652c029366d6185aa297af5a17
Bill Wang <bill.wang>
2024-02-12 15:53:56 +1100

    keycloak cluster

---
+++ clean-cluster.sh
@@ -0,0 +1,6 @@
+docker compose -f docker-compose-cluster.yml down
+
+# Clean db volume as well, when need
+# docker volume list
+# docker volume rm keycloak-compose_postgres_data
+
---
+++ start-cluster.sh
@@ -0,0 +1,3 @@
+docker compose -f docker-compose-cluster.yml up -d
+
+# open http://localhost:8180
